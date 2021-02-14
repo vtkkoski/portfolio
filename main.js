b63e64ee5157125327e19ced210027ab0f8522aa
vtkkoski <61325632+vtkkoski@users.noreply.github.com>
2021-02-14 18:02:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    $('.menu-toggler').click(function() {
+       $('.menu-toggler').toggleClass('open');
+       $('.top-nav').toggleClass('open');
+
+    });
+
+    $('.top-nav .nav-link').click(function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+ 
+     });
+
+     $('nav a[href*="#"]').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+     });
+
+     $('#up').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+     });
+
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    }); 
+
+});
